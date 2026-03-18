a08518ec6e7d67ec7c8098446d302b5ef5487df6
mrluxalot <demo170703@gmail.com>
2026-03-19 00:35:55 +0100

    feat(godot): added walk animation

--- godot/scripts/lotti.gd
+++ godot/scripts/lotti.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -170.0
 
+@onready var animated_sprite = $AnimatedSprite2D
 
+func _process(delta: float) -> void:
+	pass
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -15,11 +19,19 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk")
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("idle")
 
 	move_and_slide()
